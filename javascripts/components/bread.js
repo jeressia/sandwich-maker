0e952e341d8449a9cf1e67094a2ff9594d5684e4
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-04-11 20:12:37 -0500

    breads print to dom

---
+++ javascripts/components/bread.js
@@ -0,0 +1,13 @@
+const breadInfo = {
+    Italian: 0.50,
+    Wheat: 0.75,
+    Rye: 0.90,
+    Bun: 0.40,
+    Garlic: 0.60
+}
+
+const getBread = () => {
+    return breadInfo;
+};
+
+export default { getBread };
