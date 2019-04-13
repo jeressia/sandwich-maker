5738dfc4d6af0c3fed0b910221431847b805f3b3
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-04-13 13:41:36 -0500

    cart prints

--- javascripts/components/bread.js
+++ javascripts/components/bread.js
@@ -2,8 +2,8 @@ const breadInfo = {
     Italian: 0.50,
     Wheat: 0.75,
     Rye: 0.90,
-    Bun: 0.40,
-    Garlic: 0.60
+    Ciabatta: 0.40,
+    None: 0.60
 }
 
 const getBread = () => {
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,11 @@
 import store from './components/store.js';
 
 const init = () => {
-store.makeStore();
+store.selectBread();
+store.selectCheese();
+store.selectMeats();
+store.selectVeggies();
+store.selectCondiments();
 };
 
 init();
